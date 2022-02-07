7b1173001877ccba25b2ad726f49250387a94b50
kartikvasudev <kvasudev_be18@thapar.edu>
2022-02-07 13:40:00 +0530

    Add dot-env for database credentials

--- src/main/java/com/kartikvasudev/empmgmt/EmsApplication.java
+++ src/main/java/com/kartikvasudev/empmgmt/EmsApplication.java
@@ -1,5 +1,6 @@
 package com.kartikvasudev.empmgmt;
 
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
